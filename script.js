17c7a16c07b474f61907863d171a1ef7e00c4cc8
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-01-13 11:57:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const path = require('path');
 
-const app = express();
-
-app.use(express.static(__dirname))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
-
-  
+let input = document.getElementById("input");
+let expression = "";
+function solve(e) {
+  // console.log(e)
+  if (e === "ans") {
+    // evaluate the expression
+    let result = eval(expression);
+    console.log(result);
+    input.value = result;
+  } else if (e === "clear") {
+    expression = "";
+    input.value = "";
+  } else {
+    expression += e;
+    console.log(expression);
+    input.value = expression;
+  }
+}
